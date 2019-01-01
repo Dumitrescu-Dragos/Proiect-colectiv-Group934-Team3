b9a8116e6b34c5a42414b350f31041228137e779
dumitrescu dragos <dumitrescudragos03@gmail.com>
2019-01-01 16:07:06 +0200

    Add some DTO interfaces

--- front-end1/src/common/Client.ts
+++ front-end1/src/common/Client.ts
@@ -1,5 +1,53 @@
+import { string } from "prop-types";
 
 export type Genders = 'male' | 'female' | 'other';
 /**
  * Dtos
  */
+export interface Address{
+    id: number;
+    addressDetails?: string;
+    city?: string;
+    country?: string;
+    longitude?: number;
+    latitude?: number;
+}
+
+export interface User{
+    id: number;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    gender?: Genders;
+    DOB?: string;
+    phoneNumber?: string;
+    address?: Address;
+}
+
+export interface Category{
+    id: number;
+    name?: string;
+    description?: string;
+}
+
+export interface Tool{
+    id: number;
+    name?: string;
+    techSpec?: string;
+    isAvailable?: boolean;
+    imageUrls?: string[]; 
+}
+
+export interface Advertisement{
+    id: number;
+    title?: string;
+    description?: string;
+    type?: string;
+    address?: Address;
+    tool?: Tool;
+    ownersName?: string;
+    category?: Category;
+    periodOfTime?: number;
+    rentalConditions?: string;
+    returnRequirements?: string;
+}
