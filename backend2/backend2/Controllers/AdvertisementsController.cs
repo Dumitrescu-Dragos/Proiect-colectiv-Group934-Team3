472091b325c5621538f7203ca7976f48e3441ba6
Titus1997 <trifon.titus@yahoo.com>
2019-01-18 23:03:00 +0200

    reverted ale commit

--- backend2/backend2/Controllers/AdvertisementsController.cs
+++ backend2/backend2/Controllers/AdvertisementsController.cs
@@ -14,7 +14,8 @@ namespace backend2.Controllers
     [ApiController]
     public class AdvertisementsController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        //this will be refactored
+        private ApplicationDbContext _context;
 
         public AdvertisementsController(ApplicationDbContext context)
         {
@@ -22,91 +23,10 @@ namespace backend2.Controllers
         }
 
         // GET: api/Advertisements
-        [HttpGet("filters")]
-        public IEnumerable<Advertisement> GetAdvertisementsFiltered(Filters filters)
-        {
-            List<Advertisement> advertisements = _context.Advertisements.ToList();
-            List<Advertisement> filteredAdvertisements = new List<Advertisement>();
-            IEnumerable<User> owners = _context.Users;
-            IEnumerable<Tool> tools = _context.Tools;
-            IEnumerable<Category> categories = _context.Categories;
-            IEnumerable<Address> locations = _context.Addresses;
-            List<ToolImage> images = _context.ToolImages.ToList();
-            foreach (Advertisement a in advertisements)
-            {
-
-                a.Owner = owners.Single(x => x.Id == a.OwnerId);
-                //incluziune multipla (Advertisement contine User si User contine Advertisement)
-                a.Owner.Advertisements = null;
-                a.Tool = tools.SingleOrDefault(x => x.ToolId == a.ToolId);
-                a.Tool.Images = images.FindAll(x => x.ToolId == a.ToolId);
-                foreach (ToolImage ti in a.Tool.Images)
-                {
-                    //incluziune multipla (Tool contine ToolImage si ToolImage contine Tool)
-                    ti.Tool = null;
-                }
-                a.Category = categories.SingleOrDefault(x => x.CategoryId == a.CategoryId);
-                a.Location = locations.SingleOrDefault(x => x.AddressId == a.LocationId);
-            }
-            var comp = StringComparison.OrdinalIgnoreCase;
-            foreach (Advertisement a in advertisements)
-            {
-                if (filters.searchFilter == null || a.Title.Contains(filters.searchFilter, comp))
-                {
-                    filteredAdvertisements.Add(a);
-                }
-            }
-
-            if (filters.categoryFilter != null && filteredAdvertisements.Count() != 0)
-            {
-                foreach (Advertisement a in filteredAdvertisements.ToList())
-                {
-                    if (a.Category.Name != filters.categoryFilter)
-                    {
-                        filteredAdvertisements.Remove(a);
-                    }
-                }
-            }
-            if (filters.availableFilter == true && filteredAdvertisements.Count() != 0)
-            {
-                foreach (Advertisement a in filteredAdvertisements.ToList())
-                {
-                    if (a.Tool.IsAvailable == false)
-                    {
-                        filteredAdvertisements.Remove(a);
-                    }
-                }
-            }
-            return filteredAdvertisements;
-        }
-
-        // GET: api/Advertisements
         [HttpGet]
         public IEnumerable<Advertisement> GetAdvertisements()
         {
-            IEnumerable<Advertisement> advertisements = _context.Advertisements;
-            IEnumerable<User> owners = _context.Users;
-            IEnumerable<Tool> tools = _context.Tools;
-            IEnumerable<Category> categories = _context.Categories;
-            IEnumerable<Address> locations = _context.Addresses;
-            List<ToolImage> images = _context.ToolImages.ToList();
-            foreach (Advertisement a in advertisements)
-            {
-
-                a.Owner = owners.Single(x => x.Id == a.OwnerId);
-                //incluziune multipla (Advertisement contine User si User contine Advertisement)
-                a.Owner.Advertisements = null;
-                a.Tool = tools.SingleOrDefault(x => x.ToolId == a.ToolId);
-                a.Tool.Images = images.FindAll(x => x.ToolId == a.ToolId);
-                foreach (ToolImage ti in a.Tool.Images)
-                {
-                    //incluziune multipla (Tool contine ToolImage si ToolImage contine Tool)
-                    ti.Tool = null;
-                }
-                a.Category = categories.SingleOrDefault(x => x.CategoryId == a.CategoryId);
-                a.Location = locations.SingleOrDefault(x => x.AddressId == a.LocationId);
-            }
-            return advertisements;
+            return _context.Advertisements;
         }
 
         // GET: api/Advertisements/5
