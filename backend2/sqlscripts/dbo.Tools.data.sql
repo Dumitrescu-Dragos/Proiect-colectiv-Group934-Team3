e439a5ce8c51846401bf6603ed2c04fc9e6ac922
Alexandra Maior <amaior77@gmail.com>
2019-01-18 14:48:48 +0200

    Added CategoriesController, Added filters for advertisements, small change to script

--- backend2/sqlscripts/dbo.Tools.data.sql
+++ backend2/sqlscripts/dbo.Tools.data.sql
@@ -1,4 +1,6 @@
-﻿  SET IDENTITY_INSERT [dbo].[Tools] ON
+﻿delete from [dbo].[Tools]
+
+  SET IDENTITY_INSERT [dbo].[Tools] ON
 Insert Into [dbo].[Tools] ([ToolId], [Name], [TechSpecs], [IsAvailable]) values (1,'Slefuitor Bosch' ,'Putere: 900 W \nRotatii in gol: 2.800–1.000/min \nDiametru disc: 125 mm', 1) 
  Insert Into [dbo].[Tools] ([ToolId], [Name], [TechSpecs], [IsAvailable]) values (2,'Masina de gaurit Bosch' ,'Putere nominala: 790 W \nTuratie in gol: 0 - 930/min. \nImpact: 2,7 J', 1) 
  Insert Into [dbo].[Tools] ([ToolId], [Name], [TechSpecs], [IsAvailable]) values (3,'Telemetru cu laser Bosch' ,'Domeniu de măsurare: 0,15 – 30,00 \nPrecizie de măsurare, normal: +/- 2 mm \nTimp de măsurare maxim.: 4 s', 1) 
@@ -8,6 +10,7 @@ Insert Into [dbo].[Tools] ([ToolId], [Name], [TechSpecs], [IsAvailable]) values 
  Insert Into [dbo].[Tools] ([ToolId], [Name], [TechSpecs], [IsAvailable]) values (7,'Nivela cu laser in cruce Leica' ,'Nivela cu laser in cruce\n+/- 3 & nbsp;\nVizibilitate interioară 25 m', 0) 
  SET IDENTITY_INSERT [dbo].[Tools] OFF
  
+ delete from [dbo].[ToolImages]
  SET IDENTITY_INSERT [dbo].[ToolImages] ON
 
  Insert Into [dbo].[ToolImages] ([ToolImageId], [ImageUrl], [ToolId]) values (1,'https://static11.edstatic.net/product_images/470x470/resize/angle-grinder-gws-9-115-144024-0601396007_nvn59cve.png?v=1',1)
@@ -19,6 +22,7 @@ Insert Into [dbo].[Tools] ([ToolId], [Name], [TechSpecs], [IsAvailable]) values 
  Insert Into [dbo].[ToolImages] ([ToolImageId], [ImageUrl], [ToolId]) values (7,'https://static9.edstatic.net/product_images/470x470/resize/l2leftreflectionc741305a1opng_rp3ff3ry.png?v=1',7)
  SET IDENTITY_INSERT [dbo].[ToolImages] OFF
 
+ delete from [dbo].[Addresses]
  SET IDENTITY_INSERT [dbo].[Addresses] ON
 INSERT INTO [dbo].[Addresses] ([AddressId], [AddressString], [City], [Country], [Longitude], [Latitude]) VALUES (1, N'Septimiu Albini 21', N'Cluj-Napoca', N'Romania', 101, 111)
 INSERT INTO [dbo].[Addresses] ([AddressId], [AddressString], [City], [Country], [Longitude], [Latitude]) VALUES (2, N'Paris 2', N'Cluj-Napoca', N'Romania', 55, 4)
@@ -29,6 +33,7 @@ INSERT INTO [dbo].[Addresses] ([AddressId], [AddressString], [City], [Country], 
 INSERT INTO [dbo].[Addresses] ([AddressId], [AddressString], [City], [Country], [Longitude], [Latitude]) VALUES (7, N'B. P. Hasdeu 50', N'Cluj-Napoca', N'Romania', 232, 532)
 SET IDENTITY_INSERT [dbo].[Addresses] OFF
 
+delete from [dbo].[Users]
  SET IDENTITY_INSERT [dbo].[Users] ON
 INSERT INTO [dbo].[Users] ([Id], [Email], [Password], [FirstName], [LastName], [Gender], [DateOfBirth], [PhoneNumber], [AddressId]) VALUES (1, 'amaior@tools.com', 'maior', 'Alexandra', 'Maior', '2', '1990-07-01', '0758962145', 1)
 INSERT INTO [dbo].[Users] ([Id], [Email], [Password], [FirstName], [LastName], [Gender], [DateOfBirth], [PhoneNumber], [AddressId]) VALUES (2, 'atili@tools.com', 'tili', 'Adrian', 'Tili', '1', '1992-06-05', '0757565545', 2)
@@ -39,6 +44,7 @@ INSERT INTO [dbo].[Users] ([Id], [Email], [Password], [FirstName], [LastName], [
 INSERT INTO [dbo].[Users] ([Id], [Email], [Password], [FirstName], [LastName], [Gender], [DateOfBirth], [PhoneNumber], [AddressId]) VALUES (7, 'bmaier@tools.com', 'maier', 'Bogdan', 'Maier', '1', '1990-12-12', '0755789520', 7)
 SET IDENTITY_INSERT [dbo].[Users] OFF
 
+delete from [dbo].[Categories]
 SET IDENTITY_INSERT [dbo].[Categories] ON
 INSERT INTO [dbo].[Categories] ([CategoryId], [Name], [Description]) VALUES (1, 'Aparate de masura', 'contine aparate folosite la efectuarea de masuratori precum telemetre si nivele laser')
 INSERT INTO [dbo].[Categories] ([CategoryId], [Name], [Description]) VALUES (2, 'Scule cu acumulator', 'contine masini de gaurit si insurubat cu acumulator')
@@ -47,6 +53,7 @@ INSERT INTO [dbo].[Categories] ([CategoryId], [Name], [Description]) VALUES (4, 
 INSERT INTO [dbo].[Categories] ([CategoryId], [Name], [Description]) VALUES (5, 'Fierastraie', 'contine fierastraie sabie, fierastraie circulare, fierastraie pendulare')
 SET IDENTITY_INSERT [dbo].[Categories] OFF
 
+delete from [dbo].[Advertisements]
 SET IDENTITY_INSERT [dbo].[Advertisements] ON
 INSERT INTO [dbo].[Advertisements] ([Id], [Title], [Description], [Type], [PeriodOfTime], [RentalConditions], [ReturnRequirements], [OwnerId], [ToolId], [CategoryId], [LocationId]) VALUES 
 									(1, 'Ofer spre imprumut masina de gaurit / insurubat cu acumulator Bosch GSR 120-LI 12V Professional', 'Bosch GSR 120-LI este o masina de gaurit si insurubat de inalta performanta, oferind cu 20% imbunatatiri ale cuplului, comparativ cu GSR 1080-2-LI.
@@ -66,6 +73,7 @@ INSERT INTO [dbo].[Advertisements] ([Id], [Title], [Description], [Type], [Perio
 
 SET IDENTITY_INSERT [dbo].[Advertisements] OFF
 
+delete from [dbo].[Notifications]
 SET IDENTITY_INSERT [dbo].[Notifications] ON
 INSERT INTO [dbo].[Notifications] ([NotificationId], [email], [AdvertisementId]) VALUES (1, 'bmaier@tools.com', 1)
 INSERT INTO [dbo].[Notifications] ([NotificationId], [email], [AdvertisementId]) VALUES (2, 'bmaier@tools.com', 2)
@@ -73,6 +81,7 @@ INSERT INTO [dbo].[Notifications] ([NotificationId], [email], [AdvertisementId])
 INSERT INTO [dbo].[Notifications] ([NotificationId], [email], [AdvertisementId]) VALUES (4, 'ttrifon@tools.com', 4)
 SET IDENTITY_INSERT [dbo].[Notifications] OFF
 
+delete from [dbo].[Rentals]
 SET IDENTITY_INSERT [dbo].[Rentals] ON
 INSERT INTO [dbo].[Rentals] ([RentalId], [StartTime], [EndTime], [IntialState], [FinalState], [AdvertisementId]) VALUES (1, '2018-05-05', '2018-05-10', '3', '3', 5)
 INSERT INTO [dbo].[Rentals] ([RentalId], [StartTime], [EndTime], [IntialState], [FinalState], [AdvertisementId]) VALUES (2, '2018-09-10', '2018-09-15', '3', '2', 5)
@@ -84,12 +93,14 @@ INSERT INTO [dbo].[Rentals] ([RentalId], [StartTime], [EndTime], [IntialState], 
 INSERT INTO [dbo].[Rentals] ([RentalId], [StartTime], [EndTime], [IntialState], [FinalState], [AdvertisementId]) VALUES (8, '2019-01-09', '2019-01-09', '3', '3', 4)
 SET IDENTITY_INSERT [dbo].[Rentals] OFF
 
+delete from [dbo].[Reviews]
 SET IDENTITY_INSERT [dbo].[Reviews] ON
 INSERT INTO [dbo].[Reviews] ([ReviewId], [Text], [Rating], [AdvertisementId]) VALUES (1, 'Masina s-a comportat exemplar, iar comunicarea cu propietarul a fost foarte buna.', 5, 5)
 INSERT INTO [dbo].[Reviews] ([ReviewId], [Text], [Rating], [AdvertisementId]) VALUES (2, 'Buna.', 4, 5)
 INSERT INTO [dbo].[Reviews] ([ReviewId], [Text], [Rating], [AdvertisementId]) VALUES (3, 'Exact cum zice in descriere. Mi-a fost de real folos.', 5, 6)
 INSERT INTO [dbo].[Reviews] ([ReviewId], [Text], [Rating], [AdvertisementId]) VALUES (4, 'Masina in stare buna, iar proprietarul foarte deschis - mi-a oferit si niste sfaturi.', 5, 1)
 SET IDENTITY_INSERT [dbo].[Reviews] OFF
+
 
 INSERT INTO [dbo].[Invites]([InviteId]) VALUES ('inviteid1')
 INSERT INTO [dbo].[Invites]([InviteId]) VALUES ('inviteid2')
