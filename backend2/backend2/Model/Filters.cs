c46335ce4996ca38d859c9ae08670d648d8a5a40
Titus1997 <trifon.titus@yahoo.com>
2019-01-18 20:36:05 +0200

    nu mai inteleg nimic

---
+++ backend2/backend2/Model/Filters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend2.Model
+{
+    public class Filters
+    {
+        public string searchFilter;
+        public string categoryFilter;
+        public bool availableFilter;
+    }
+}
