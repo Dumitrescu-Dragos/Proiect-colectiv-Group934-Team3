1d3079beed24031d63bb6d9a352a73f05063ead6
Titus1997 <trifon.titus@yahoo.com>
2019-01-20 00:04:15 +0200

    Just started giving it shape(wanted to elt you know)

--- front-end1/src/views/landingPage/LandingPage.tsx
+++ front-end1/src/views/landingPage/LandingPage.tsx
@@ -35,9 +35,9 @@ export default class LandingPage extends React.Component<ILandingPageProps, ILan
                 <Header />
                 <div className='container'>
                     <div className='left landing-page-content'>
-                        <h1> TITLE HERE </h1>
-                        <h3> Greetings </h3>
-                        <p> content here .... The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk. A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just</p>
+                        <h1> Hello there </h1>
+                        <h3> Welcome to OSC </h3>
+                        <p> OSC is a magnificent web application that is designed for a small, honest group of users. It helps its users find certain tools nearby and rent them. Also OSC allows its users to share their own tools.</p>
                     </div>
                     {this.renderAuthorization()}
                 </div>
