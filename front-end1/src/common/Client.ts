53154ba064e8920b9680c92536af5e63886fdf0d
unknown <bogdanmaier49@gmail.com>
2019-01-19 15:34:54 +0200

    Added Http request in FrontEnd to get AllAds

--- front-end1/src/common/Client.ts
+++ front-end1/src/common/Client.ts
@@ -3,8 +3,8 @@ import { string } from "prop-types";
 export type Genders = 'male' | 'female' | 'other';
 
 const baseUrl: string = 'https://localhost:44331/api';
-export const getAdsUrl: string = baseUrl + "/advertisements/filters" ;
-export const getCategoriesUrl: string = baseUrl + "/categories"
+export const getAdsUrl: string = baseUrl + "/Advertisements" ;
+export const getCategoriesUrl: string = baseUrl + "/categories";
 /**
  * Dtos
  */
